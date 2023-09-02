a5d2341cbc8333b55cfcee79da153cbd6c3dd861
htx2v <huynhvy.dev@gmail.com>
2023-09-02 18:20:11 +0700

    fetch and display vans data from server.js

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import { createServer } from "./server"
 import App from './App.jsx'
 import './index.css'
 
--- react-router/src/server.js
+++ react-router/src/server.js
@@ -28,3 +28,5 @@ createServer({
         })
     }
 })
+
+export { createServer };
