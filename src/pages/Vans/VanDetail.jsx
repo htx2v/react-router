04c18121de8f79412f9fef3c1bc6fb1f697cfd12
htx2v <huynhvy.dev@gmail.com>
2023-10-11 19:08:03 +0700

    feat: automatically update "Back to all vans" button label refs: #58

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -15,6 +15,7 @@ export default function VanDetail() {
 
     // const search = location.state && location.state.search || ""
     const search = location.state?.search || ""
+    const type = location.state?.type || "all"
 
     return (
         <div className="van-detail-container">
@@ -23,7 +24,7 @@ export default function VanDetail() {
             relative="path"
             className="back-button"
         >
-            &larr; <span>Back to all vans</span>
+            &larr; <span>Back to {type} vans</span>
         </Link>
             {van ? (
                 <div className="van-detail">
