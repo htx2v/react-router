6754e09290a6a269356d869b367abda4a8676a57
htx2v <huynhvy.dev@gmail.com>
2023-10-24 00:04:50 +0700

    feat: custom initial page redirect after login refs: #113-114

--- react-router/src/utils.js
+++ react-router/src/utils.js
@@ -1,10 +1,11 @@
 import { redirect } from "react-router-dom"
 
-export async function requireAuth() {
+export async function requireAuth(request) {
     const isLoggedIn = localStorage.getItem("loggedin")
+    const pathName = new URL(request.url).pathname
 
     if (!isLoggedIn) {
-        const response = redirect("/login?message=You must login first")
+        const response = redirect(`/login?message=You must login first&redirectTo=${pathName}`)
         response.body = true
         throw response
     }
